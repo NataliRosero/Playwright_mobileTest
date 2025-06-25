921c61ddb9e4bfb603f3ca15fb56c45b21b4a40a
NataliRosero <natali.rosero@gmail.com>
2025-06-25 14:32:27 -0500

    automatizacion flujo de inicio de sesion y validación de ingreso

--- page/login.js
+++ page/login.js
@@ -2,12 +2,13 @@ exports.LoginPage = class LoginPage {
 
       constructor(page) {
 
-
         this.page = page
         this.username =   page.getByRole('textbox', { name: 'Email or phone' })
         this.buttonNext = page.getByRole('button', { name: 'Next' })
         this.password = page.getByRole('textbox', { name: 'Enter your password' })
         this.buttonNextPass = page.getByRole('button', { name: 'Next' })
+        this.validateMessage = page.locator ('//header[contains(@class,\'R3PZhd\')]//h1[contains(@class,\'XY0ASe\')]')
+
       }
 
        async gotoPage() {
@@ -16,15 +17,18 @@ exports.LoginPage = class LoginPage {
 
      async signIn(username, password){
 
-        await this.username.click()
         await this.username.fill(username)
         await this.buttonNext.click()
-        await this.password.click()
         await this.password.fill(password)
-        await this.buttonNextPass.click()
-        await this.page.waitForTimeout(5000);    
+        await this.buttonNextPass.click()       
      }
 
-     
+     async validateSignIn(){
+      
+        await this.page.waitForTimeout(5000)       
+        const title = await this.validateMessage.innerText();
+        console.log('Mensaje inicio sesion:', title);
+
+     }
 
 }
